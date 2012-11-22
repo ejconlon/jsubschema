7509db2cc078e3434ef677a56395cfda5090c4bb
Eric Conlon <ejconlon@gmail.com>
2012-11-22 05:34:40 -0800

    better pathing

--- src/main/java/net/exathunk/jsubschema/base/Pather.java
+++ src/main/java/net/exathunk/jsubschema/base/Pather.java
@@ -18,6 +18,7 @@ public class Pather {
     }
 
     private static Either3<SchemaRef, String, PointedRef> pathSchemaInner(SchemaRef schemaRef, SchemaRef rootRef, Pointer pointer) {
+        assert Direction.UP.equals(pointer.getDirection());
         if (schemaRef.getSchema().has__dollar__ref()) {
             Either<Reference, String> eitherReference = Reference.fromReferenceString(schemaRef.getSchema().get__dollar__ref());
             if (eitherReference.isSecond()) return Either3.makeSecond(eitherReference.getSecond());
@@ -64,11 +65,10 @@ public class Pather {
     }
 
     public static Either3<SchemaRef, String, PointedRef> pathDomain(SchemaRef schemaRef, Pointer pointer) {
-        assert (Direction.UP.equals(pointer.getDirection()));
-        return unDollarRef(schemaRef, schemaRef, pointer);
+        return pathDomainInner(schemaRef, schemaRef, pointer);
     }
 
-    private static Either3<SchemaRef, String, PointedRef> unDollarRef(SchemaRef schemaRef, SchemaRef rootRef, Pointer pointer) {
+    private static Either3<SchemaRef, String, PointedRef> pathDomainInner(SchemaRef schemaRef, SchemaRef rootRef, Pointer pointer) {
         assert Direction.UP.equals(pointer.getDirection());
         if (schemaRef.getSchema().has__dollar__ref()) {
             Either<Reference, String> eitherReference = Reference.fromReferenceString(schemaRef.getSchema().get__dollar__ref());
@@ -78,23 +78,12 @@ public class Pather {
                 // If this is a relative ref and we have an abs path we're following, set the abs path in the rel ref
                 reference = new Reference(schemaRef.getReference().getUrl(), reference.getPointer());
             }
-            // Add the rest of the parts we havent reached, then return the ref
             if (reference.getUrl().equals(schemaRef.getReference().getUrl())) {
-                if (schemaRef == rootRef) return Either3.makeSecond("Circular ref from: "+ schemaRef.getReference().toReferenceString()+";"+pointer.toPointerString());
-                Pointer localRef = reference.getPointer().reversed();
-                if (localRef.size() == 0) {
-                    return unDollarRef(rootRef, rootRef, pointer);
-                } if (localRef.size() != 2 || !localRef.getHead().equals(Part.asKey("declarations")) || !localRef.getTail().getHead().hasKey() ||
-                    !rootRef.getSchema().hasDeclarations() || !rootRef.getSchema().getDeclarations().containsKey(localRef.getTail().getHead().getKey())) {
-                    return Either3.makeSecond("Bad local reference: "+reference.toReferenceString());
-                } else {
-                    final String nextKey = localRef.getTail().getHead().getKey();
-                    final SchemaLike nextSchema = rootRef.getSchema().getDeclarations().get(nextKey);
-                    final Reference nextRef = rootRef.getReference().cons(Part.asKey("declarations")).cons(Part.asKey(nextKey));
-                    return unDollarRef(new SchemaRef(nextSchema, nextRef), rootRef, pointer);
-                }
+                Either3<SchemaRef, String, PointedRef> x = pathSchemaInner(schemaRef, rootRef, reference.getPointer().reversed());
+                if (x.isFirst()) return pathDomainInner(x.getFirst(), rootRef, pointer);
+                else return x;
+
             } else {
-                // non-local ref - return so resolvers can pick up
                 return Either3.makeThird(new PointedRef(reference, pointer));
             }
         } else if (pointer.isEmpty()) {
@@ -106,21 +95,21 @@ public class Pather {
                 if (schema.hasItems()) {
                     final SchemaLike nextSchema = schema.getItems();
                     final Reference nextRef = schemaRef.getReference().cons(Part.asKey("items"));
-                    return unDollarRef(new SchemaRef(nextSchema, nextRef), rootRef, pointer.getTail());
+                    return pathDomainInner(new SchemaRef(nextSchema, nextRef), rootRef, pointer.getTail());
                 } else if (schema.hasProperties() && schema.getProperties().containsKey(part.getKey())) {
                     final SchemaLike nextSchema = schema.getProperties().get(part.getKey());
                     final Reference nextRef = schemaRef.getReference().cons(Part.asKey("properties")).cons(part);
-                    return unDollarRef(new SchemaRef(nextSchema, nextRef), rootRef, pointer.getTail());
+                    return pathDomainInner(new SchemaRef(nextSchema, nextRef), rootRef, pointer.getTail());
                 } else {
                     return Either3.makeSecond("pathDomain expected object: "+schemaRef.getReference().toReferenceString()+";"+pointer.toPointerString());
                 }
             } else {
                 if (!schema.getType().equals("array") || schema.getItems() == null) {
-                    return Either3.makeSecond("pathDomain expected array: " + schemaRef.getReference().toReferenceString() + ";" + pointer.toPointerString());
+                    return Either3.makeSecond("pathDomain expected array: "+schemaRef.getReference().toReferenceString()+";"+pointer.toPointerString());
                 } else {
                     final SchemaLike nextSchema = schema.getItems();
                     final Reference nextRef = schemaRef.getReference().cons(Part.asKey("items"));
-                    return unDollarRef(new SchemaRef(nextSchema, nextRef), rootRef, pointer.getTail());
+                    return pathDomainInner(new SchemaRef(nextSchema, nextRef), rootRef, pointer.getTail());
                 }
             }
         }
